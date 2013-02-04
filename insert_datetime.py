35c756473fefb3470fec4c5f23895384b5342f93
leon <xhl@live.it>
2013-02-04 23:01:42 +0800

    add ST3 support

--- insert_datetime.py
+++ insert_datetime.py
@@ -1,15 +1,20 @@
 # coding=utf-8
-import sublime_plugin, datetime
+# 比较版本号
+# import sys;print(sys.version_info[:2] < (3,0))
+# import platform;print(platform.python_version() < '3.0')
+# print(int(sublime.version()) < 3000)
+
+import sys, sublime_plugin, datetime
 
 class insertDatetimeCommand(sublime_plugin.TextCommand):
   def run(self, edit, format):
     timestamp = datetime.datetime.now()
-    if format == "ymd":
+    if format == 'ymd':
         # yyyy-mm-dd
-        timestamp = timestamp.strftime("%Y-%m-%d")
-    elif format == "ymdhms":
+        timestamp = timestamp.strftime('%Y-%m-%d')
+    elif format == 'ymdhms':
         # %X = %H:%M:%S
-        timestamp = timestamp.strftime("%Y-%m-%d %X")
+        timestamp = timestamp.strftime('%Y-%m-%d %X')
     else: # format == "xxx"
         # 2012-02-18 13:17:28.047000
         #timestamp = datetime.datetime.now().isoformat(' ')
@@ -19,12 +24,17 @@ class insertDatetimeCommand(sublime_plugin.TextCommand):
         # 数字变为字符串 str(xx),字符串变为数字 int(string)
         timestamp = int(timestamp.strftime("%w"))
         week = {
-            1 : "一", 2 : "二", 3 : "三", 4 : "四", 5 : "五", 6 : "六", 7 : "日"
+            1 : '一', 2 : '二', 3 : '三', 4 : '四', 5 : '五', 6 : '六', 7 : '日'
         }
 
-        timestamp = "星期" + week[timestamp]
-        ## 中文要指定: coding=utf-8 | gbk ，再decode
-        timestamp = timestamp.decode("utf-8")
+        timestamp = '星期' + week[timestamp]
+        
+        # print(sys.version_info[:2] < (3,0))
+        if sys.version_info[:2] < (3,0):
+            ## 中文要指定: coding=utf-8 | gbk ，再decode
+            timestamp = timestamp.decode('utf-8')
+        # else:
+            # timestamp = timestamp.encode('utf-8').decode('utf-8')
 
     #for region in the selection
     for r in self.view.sel():
